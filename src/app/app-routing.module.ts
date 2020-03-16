8b629abaa5fa84f2e161255697939df33a632e83
naphattharawat <naphattharawat@gmail.com>
2020-03-16 18:22:23 +0700

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from 'src/app/home/home.component';
-import { LayoutComponent } from 'src/app/layout/layout.component';
 
 
 const routes: Routes = [
