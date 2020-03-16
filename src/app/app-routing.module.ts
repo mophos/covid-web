8e42ad182a13181bfa75a2337142ec3ee9c8a366
peemppk <peemppk5538@gmail.com>
2020-03-16 11:16:15 +0700

    push

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from 'src/app/home/home.component';
+import { LayoutComponent } from 'src/app/layout/layout.component';
 
+const routes: Routes = [{
+  path: '',
+  component: LayoutComponent,
+  children: [
+    // { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
 
-const routes: Routes = [];
+  ]
+}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    onSameUrlNavigation: 'reload'
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { HomeComponent } from 'src/app/home/home.component';
+import { LayoutComponent } from 'src/app/layout/layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    HomeComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [HomeComponent]
 })
 export class AppModule { }
