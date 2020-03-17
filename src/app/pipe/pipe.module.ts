7bed45db54214a173f5db034107c1651b9acf020
naphattharawat <naphattharawat@gmail.com>
2020-03-17 15:49:35 +0700

    pipe

---
+++ src/app/pipe/pipe.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DatetimePipe } from './datetime.pipe';
+
+
+
+@NgModule({
+  declarations: [DatetimePipe],
+  imports: [
+    CommonModule
+  ]
+})
+export class PipeModule { }
