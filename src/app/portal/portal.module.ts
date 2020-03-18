6d327ac5ffbe100d83514383d39d5759f20a02e2
naphattharawat <naphattharawat@gmail.com>
2020-03-18 10:40:15 +0700

    update

--- src/app/portal/portal.module.ts
+++ src/app/portal/portal.module.ts
@@ -6,15 +6,13 @@ import { CommonModule } from '@angular/common';
 import { PortalRoutingModule } from './portal-routing.module';
 import { LayoutComponent } from './layout/layout.component';
 import { HomeComponent } from './home/home.component';
-import { AngularFullpageModule } from '@fullpage/angular-fullpage';
 
 @NgModule({
   declarations: [LayoutComponent, HomeComponent],
   imports: [
     CommonModule,
     PortalRoutingModule,
-    PipeModule,
-    AngularFullpageModule
+    PipeModule
   ],
   providers: [
     ApiService
